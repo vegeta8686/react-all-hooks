a356f9179f223b6c92eb959613a8140314c9fb72
ImVk <vamsivegeta@gmail.com>
2023-02-03 17:39:20 +0530

    changed extension from js to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Usestate from './components/UsestateFnc';
 import { CreateTodo } from './components/useReducer/CreateTodo';
 import { Counter } from './components/useReducer/Counter';
+import { Useeffect } from './components/Useeffect';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/useState" element={<Usestate />} />
           <Route path="/counter" element={<Counter />} />
           <Route path="/todo" element={<CreateTodo />} />
+          <Route path="/useEffect" element={<Useeffect />} />
         </Routes>
       </Router>
     </div>
